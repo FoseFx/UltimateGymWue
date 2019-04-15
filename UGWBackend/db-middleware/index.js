57eb271dd9f41816273ef7bd9ee6c357a1203756
FoseFx <fosefx@pm.me>
2019-04-15 19:50:56 +0200

    [New] [Backend] [DB] added registerUser

--- UGWBackend/db-middleware/index.js
+++ UGWBackend/db-middleware/index.js
@@ -10,9 +10,33 @@ let db = admin.firestore();
 
 http.createServer((req, res) => {
 
+  if (req.headers['authorization'] !== process.env['SECRET']){
+    res.statusCode = 401;
+    return res.end("Unauthorized");
+  }
+
+  if (/^\/register\/.*$/.test(req.url)) {
+    return registerUser(req, res);
+  }
     
 
-    res.end();
+  res.end();
 }).listen(8080, () => {
     console.log("DB Middleware server listening on port 8080");
 });
+
+function registerUser(req, res){
+  
+  let base = req.url.replace("/register/", "").replace("/", "");
+  let as_string = Buffer.from(base, 'base64').toString('ascii');
+  let as_obj = JSON.parse(as_string);
+
+  console.log(as_obj);
+  db.collection('users').add(as_obj).then(ref => {
+    res.end("" + ref.id);
+  }).catch((err) => {
+    console.error(err);
+    res.end("err");
+  });
+  
+}
