3daa80a79f16f917bc5b4f0228eb7abe87d97701
FoseFx <fosefx@pm.me>
2019-04-15 14:38:51 +0200

    [New] [Backend] [Auth] [Normal] New Handler

--- UGWBackend/src/auth/normal/normal.rs
+++ UGWBackend/src/auth/normal/normal.rs
@@ -4,6 +4,10 @@
 extern crate rand;
 use super::passw;
 use rocket_contrib::json::{Json};
+use rocket::response::status::Custom;
+use rocket::response::status;
+use rocket::http::Status;
+use super::super::regexes;
 
 #[derive(Deserialize)]
 #[derive(Debug)]
@@ -14,6 +18,18 @@ pub struct NormalRegisterData {
 }
 
 #[post("/auth/normal/register", data = "<data>")]
-pub fn normal_handler(data: Json<NormalRegisterData>) -> String {
-    return format!("{:?}", passw::hash_passw(&data.password));
+pub fn normal_handler(data: Json<NormalRegisterData>) -> status::Custom<String> {
+    if !regexes::is_valid_name(&data.fullname) {
+       return status::Custom(Status::BadRequest, format!("Fullname not valid"));
+    }
+    if !regexes::is_valid_email(&data.email) {
+       return status::Custom(Status::BadRequest, format!("Email not valid"));
+    }
+
+    // password enforcment here
+
+    let (hash, salt) = passw::hash_passw(&data.password);
+
+    return status::Custom(Status::Ok, format!("Ok"));
+    
 }
