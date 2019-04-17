4aca0dcf3f670bc844905c50e3c88f8906fe52f7
FoseFx <fosefx@pm.me>
2019-04-17 13:31:29 +0200

    [New] [Frontend] Added Spinner

--- UGWFrontend/src/app/modules/ui/card/card.component.ts
+++ UGWFrontend/src/app/modules/ui/card/card.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() disabled = false;
+  @Input() loading = false;
   constructor() { }
 
   ngOnInit() {
--- UGWFrontend/src/app/modules/ui/ui.module.ts
+++ UGWFrontend/src/app/modules/ui/ui.module.ts
@@ -4,13 +4,15 @@ import { ButtonComponent } from './button/button.component';
 import { CardComponent } from './card/card.component';
 import { InputComponent } from './input/input.component';
 import { HrComponent } from './hr/hr.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     CardComponent,
     InputComponent,
-    HrComponent
+    HrComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
