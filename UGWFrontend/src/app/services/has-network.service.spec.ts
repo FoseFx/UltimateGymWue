9b3bb655d48e975335ca64db6db1c5cf2bc110d8
FoseFx <fosefx@pm.me>
2019-06-22 13:10:13 +0200

    Added tests for hasNetworkService

--- UGWFrontend/src/app/services/has-network.service.spec.ts
+++ UGWFrontend/src/app/services/has-network.service.spec.ts
@@ -1,6 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 
 import {HasNetworkService} from './has-network.service';
+import {BehaviorSubject} from "rxjs";
 
 describe('HasNetworkService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,36 @@ describe('HasNetworkService', () => {
   it('should be created', () => {
     const service: HasNetworkService = TestBed.get(HasNetworkService);
     expect(service).toBeTruthy();
+  });
+  describe('isOnline', () => {
+    it('should return false', () => {
+      const service: HasNetworkService = TestBed.get(HasNetworkService);
+      service.isOnline$.next(false);
+      console.log('online: ', service.isOnline$.getValue());
+      expect(service.isOnline).toEqual(false);
+    });
+    it('should return true', () => {
+      const service: HasNetworkService = TestBed.get(HasNetworkService);
+      service.isOnline$.next(true);
+      expect(service.isOnline).toEqual(true);
+    });
+  });
+  describe('constructor', () => {
+    it('should register listener for ononline', () => {
+      const service: HasNetworkService = TestBed.get(HasNetworkService);
+      const onlineEvent = new Event('online');
+      service.isOnline$.next(false);
+      expect(service.isOnline).toEqual(false);
+      window.dispatchEvent(onlineEvent);
+      expect(service.isOnline).toEqual(true);
+    });
+    it('should register listener for onoffline', () => {
+      const service: HasNetworkService = TestBed.get(HasNetworkService);
+      const offlineEvent = new Event('offline');
+      service.isOnline$.next(true);
+      expect(service.isOnline).toEqual(true);
+      window.dispatchEvent(offlineEvent);
+      expect(service.isOnline).toEqual(false);
+    });
   });
 });
