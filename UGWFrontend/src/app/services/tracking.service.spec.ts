3e45ab97df6534c1fd13c3cc77875d46dee08dcd
FoseFx <fosefx@pm.me>
2019-07-12 13:27:47 +0200

    Added tests

--- UGWFrontend/src/app/services/tracking.service.spec.ts
+++ UGWFrontend/src/app/services/tracking.service.spec.ts
@@ -10,4 +10,25 @@ describe('TrackingService', () => {
     const service: TrackingService = TestBed.get(TrackingService);
     expect(service).toBeTruthy();
   });
+
+  it('should giveConsent', () => {
+    window._paq = [];
+    const service: TrackingService = TestBed.get(TrackingService);
+    service.giveConsent();
+    expect(window._paq).toEqual([['rememberConsentGiven']]);
+  });
+  it('should revokeConsent', () => {
+    window._paq = [];
+    const service: TrackingService = TestBed.get(TrackingService);
+    let called = false;
+    service.snackbarService.addSnackbar = (msg, ttl, type) => {
+      called = true;
+      expect(msg).toEqual('Das Sammeln von Verhaltensdaten wurde gestoppt.');
+      expect(ttl).toEqual(2000);
+      expect(type).toEqual('warning');
+    };
+    service.revokeConsent();
+    expect(window._paq).toEqual([['forgetConsentGiven']]);
+    expect(called).toEqual(true);
+  });
 });
