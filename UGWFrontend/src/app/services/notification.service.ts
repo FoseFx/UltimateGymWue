c2c5150adda7b2a19d5afa70f29ed12cb70f68c8
FoseFx <fosefx@pm.me>
2019-07-21 18:42:04 +0200

    Added Frontend request to backend

--- UGWFrontend/src/app/services/notification.service.ts
+++ UGWFrontend/src/app/services/notification.service.ts
@@ -1,6 +1,8 @@
 import {Injectable, Optional} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {SwPush} from '@angular/service-worker';
+import {HttpClient} from '@angular/common/http';
+import {AppQuery} from '../state/app.query';
 
 @Injectable()
 export class NotificationService {
@@ -8,7 +10,7 @@ export class NotificationService {
   navRef = navigator;
   windowRef = window;
   subscription: PushSubscription = null;
-  constructor(@Optional() public swPush: SwPush) {
+  constructor(@Optional() public swPush: SwPush, public http: HttpClient, public appQuery: AppQuery) {
     if (this.swPush) {
       this.swPush.subscription.subscribe((subs: PushSubscription) => {
         this.subscription = subs;
@@ -29,12 +31,17 @@ export class NotificationService {
   }
 
   async subscribeToPush() {
-    if (!this.canPush()) {return false;}
+    if (!this.canPush()) {return false; }
     try {
       const sub = await this.swPush.requestSubscription({
         serverPublicKey: environment.pushPublicKey
       });
-      // todo send to server
+      const resp = await this.http.post(environment.urls.newPushSub, sub.toJSON(), {
+        headers: {
+          Authorization: this.appQuery.loginToken,
+          'X-GW-Auth': this.appQuery.credentialsToken
+        }
+      }).toPromise();
       return true;
     } catch (e) {
       console.error('Could not subscribe', e);
