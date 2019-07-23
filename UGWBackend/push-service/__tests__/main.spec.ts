8adfc88e84e9727b28e27abc566d8bde539309c0
FoseFx <fosefx@pm.me>
2019-07-23 11:41:47 +0200

    Added Cron

--- UGWBackend/push-service/__tests__/main.spec.ts
+++ UGWBackend/push-service/__tests__/main.spec.ts
@@ -1,3 +1,4 @@
-it('should work', () => {
-  expect(true).toBe(true);
+import { cronPattern } from '../src/main';
+it('should set cronPattern', () => {
+  expect(cronPattern).toBe("*/5 7-20 * * 1-5");
 });
