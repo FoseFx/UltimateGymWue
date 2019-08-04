3d2bc33dcd0c86c63a82e57585b71df04bcd7478
FoseFx <fosefx@pm.me>
2019-08-04 11:12:15 +0200

    I just lost an hour worth of work

--- UGWFrontend/src/app/modules/setup/register/insta/insta.component.spec.ts
+++ UGWFrontend/src/app/modules/setup/register/insta/insta.component.spec.ts
@@ -10,6 +10,8 @@ import {AppStore} from '../../../../state/app.store';
 import {AppQuery} from '../../../../state/app.query';
 import {RouterTestingModule} from '@angular/router/testing';
 import {SetupService} from '../../state/setup.service';
+import {environment} from '../../../../../environments/environment';
+import {of} from 'rxjs';
 
 describe('InstaComponent', () => {
   let component: InstaComponent;
@@ -33,4 +35,114 @@ describe('InstaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create new window', () => {
+    // @ts-ignore
+    const fakeScreen: Screen = {width: 1920, height: 1080};
+    const spy = spyOn(window, 'open').and.callFake((url, target, feat) => {
+      expect(url).toBe(environment.urls.registerInstaRediect);
+      expect(target).toBe('popup');
+      expect(feat).toBe('top=240,left=660,width=600,height=600');
+    });
+    component.createPopup(fakeScreen);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should handle onClick', () => {
+    const spy = spyOn(component, 'createPopup');
+    const spy2 = spyOn(component, 'startInterval');
+    component.error = 'test';
+    component.onClick();
+    expect(component.error).toBe(undefined);
+    expect(spy).toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+  });
+  it('should handle blocked popup', () => {
+    const spy = spyOn(component, 'createPopup').and.returnValue(null);
+    const spy2 = spyOn(component, 'startInterval');
+    component.error = 'test';
+    component.onClick();
+    expect(component.error).toBe(undefined);
+    expect(spy).toHaveBeenCalled();
+    expect(spy2).not.toHaveBeenCalled();
+  });
+
+  it('should set the Interval', (next) => {
+    // @ts-ignore
+    spyOn(window, 'setInterval').and.callFake((fn: () => any, to: number) => {
+      expect(to).toBe(500);
+      const spy = spyOn(window, 'clearInterval');
+      fn();
+      expect(spy).toHaveBeenCalled();
+      next();
+    });
+    // @ts-ignore
+    component.startInterval({closed: true, location: {href: 'some-str'}});
+  });
+
+  it('should close window', (next) => {
+    let called = false;
+    // @ts-ignore
+    spyOn(window, 'setInterval').and.callFake((fn: () => any, to: number) => {
+      component.loading = false;
+      expect(to).toBe(500);
+      const spy = spyOn(window, 'clearInterval');
+      const spy2 = spyOn(component, 'onData');
+      fn();
+      expect(spy).toHaveBeenCalled();
+      expect(spy2).toHaveBeenCalled();
+      expect(called).toBe(true);
+      expect(component.loading).toBe(true);
+      next();
+    });
+    // @ts-ignore
+    component.startInterval({closed: true, location: {href: 'https://sub.domain.test.com/assets/insta-redirect.html'}, close: () => called = true});
+  });
+
+  describe('inData', () => {
+    it('should show error', () => {
+      spyOn(component, 'extractQueries').and.returnValue({error: true, error_description: 'some-str'});
+      component.error = undefined;
+      component.loading = true;
+      component.onData('stubbed');
+      expect(component.error).toBe('some-str');
+      expect(component.loading).toBe(false);
+    });
+
+    it('should show error when invalid', () => {
+      spyOn(component, 'extractQueries').and.returnValue({});
+      component.error = undefined;
+      component.loading = true;
+      component.onData('stubbed');
+      expect(component.error).toBe('Instagram hat nicht wie erwartet geantwortet.');
+      expect(component.loading).toBe(false);
+    });
+
+    it('should request', () => {
+      spyOn(component, 'extractQueries').and.returnValue({code: 'some-code'});
+      spyOn(component.query, 'getValue').and.returnValue({name: 'some-name'});
+      spyOn(component.setupService, 'justRegistered');
+      spyOn(component.service, 'onLogin');
+      spyOn(component.router, 'navigate');
+      const httpSpy = spyOn(component.http, 'post').and.callFake((_, body) => {
+        expect(body.code).toBe('some-code');
+        expect(body.href).toBe('https://some-domain.domain.com/test');
+        expect(body.fullname).toBe('some-name');
+        return of({});
+      });
+      component.onData('https://some-domain.domain.com/test?q=test');
+      expect(httpSpy).toHaveBeenCalled();
+    });
+
+  });
+
+  it('should extractQueries', () => {
+    const href = 'https://some-str.com/assets/insta-redirect.html?error=true&error_description=some%20message&error_reason=reason&code=code';
+    const obj = component.extractQueries(href);
+    expect(obj.code).toBe('code');
+    expect(obj.error).toBe('true');
+    expect(obj.error_description).toBe('some%20message');
+    expect(obj.error_reason).toBe('reason');
+  });
+
 });
--- UGWFrontend/src/app/modules/setup/register/insta/insta.component.ts
+++ UGWFrontend/src/app/modules/setup/register/insta/insta.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {environment} from '../../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {SetupQuery} from '../../state/setup.query';
@@ -13,30 +13,73 @@ import {SetupService} from '../../state/setup.service';
   templateUrl: './insta.component.html',
   styleUrls: ['./insta.component.scss']
 })
-export class InstaComponent implements OnInit {
+export class InstaComponent  {
 
   loading = false;
   error: string;
 
-  constructor(private http: HttpClient,
-              private query: SetupQuery,
-              private service: AppService,
-              private router: Router,
-              private setupService: SetupService) { }
-
-  ngOnInit() {
-  }
+  constructor(public http: HttpClient,
+              public query: SetupQuery,
+              public service: AppService,
+              public router: Router,
+              public setupService: SetupService) { }
 
   onClick() {
     this.error = undefined;
-    const left = (screen.width / 2) - (600 / 2);
-    const top = (screen.height / 2) - (600 / 2);
-    const child = window.open(environment.urls.registerInstaRediect, 'popup', `top=${top},left=${left},width=${600},height=${600}`);
+
+    const child = this.createPopup(window.screen);
 
     if (child === null) {
+      // popup blocked?
       return;
     }
-    const interv = setInterval(() => {
+    this.startInterval(child);
+  }
+
+  onData(href: string) {
+    const firstPart = href.split('?')[0];
+    const queries = this.extractQueries(href);
+
+    if (!!queries.error) {
+      this.error = decodeURIComponent(queries.error_description).replace(/\+/g, ' ');
+      this.loading = false;
+      return;
+    }
+
+    if (!queries.code) {
+      this.error = 'Instagram hat nicht wie erwartet geantwortet.';
+      this.loading = false;
+      return;
+    }
+
+    const code = queries.code;
+    this.http.post(environment.urls.registerInsta, {code, href: firstPart, fullname: this.query.getValue().name}).subscribe(
+
+      (data: LoginResponse) => {
+        console.log(data);
+        this.setupService.justRegistered();
+        this.service.onLogin(data);
+        this.router.navigate(['/setup/basics']);
+
+      },
+      (error) => handleError(this, error)
+    );
+
+  }
+
+  createPopup(screen: Screen): Window {
+    const left = (screen.width / 2) - (600 / 2);
+    const top = (screen.height / 2) - (600 / 2);
+    return window.open(
+      environment.urls.registerInstaRediect,
+      'popup',
+      `top=${top},left=${left},width=${600},height=${600}`
+    );
+  }
+
+  startInterval(child: Window) {
+    let interv = -1;
+    interv = window.setInterval(() => {
       console.log('closed', child.closed);
       if (child.closed) {
         clearInterval(interv);
@@ -54,43 +97,13 @@ export class InstaComponent implements OnInit {
     }, 500);
   }
 
-  onData(href: string) {
-    const firstPart = href.split('?')[0];
+  extractQueries(href: string): {code?: string, error?: string, error_description?: string, error_reason?: string}  {
     const queryStrings = href.split('/assets/insta-redirect.html?')[1].split('&');
-    const querys: {code?: string, error?: string, error_description?: string, error_reason?: string} = {};
-
+    const querys = {};
     queryStrings.forEach((pair: string) => {
       const query = pair.split('=');
       querys[query[0]] = query[1];
     });
-    console.log(querys);
-
-
-    if (!!querys.error) {
-      this.error = decodeURIComponent(querys.error_description).replace(/\+/g, ' ');
-      this.loading = false;
-      return;
-    }
-
-    if (!querys.code) {
-      this.error = 'Instagram hat nicht wie erwartet geantwortet.';
-      this.loading = false;
-      return;
-    }
-
-    const code = querys.code;
-    this.http.post(environment.urls.registerInsta, {code, href: firstPart, fullname: this.query.getValue().name}).subscribe(
-
-      (data: LoginResponse) => {
-        console.log(data);
-        this.setupService.justRegistered();
-        this.service.onLogin(data);
-        this.router.navigate(['/setup/basics']);
-
-      },
-      (error) => handleError(this, error)
-    );
-
+    return querys;
   }
-
 }
