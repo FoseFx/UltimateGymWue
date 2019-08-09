540f0ac8348a15d321928d8937efac63abc3f1f4
FoseFx <fosefx@pm.me>
2019-08-09 11:11:46 +0200

    Added tests for Normal Register Component

--- UGWFrontend/src/app/modules/setup/register/normal/normal.component.spec.ts
+++ UGWFrontend/src/app/modules/setup/register/normal/normal.component.spec.ts
@@ -11,6 +11,8 @@ import {AppStore} from '../../../../state/app.store';
 import {AppQuery} from '../../../../state/app.query';
 import {AppService} from '../../../../state/app.service';
 import {RouterTestingModule} from '@angular/router/testing';
+import {of} from "rxjs";
+import {map} from "rxjs/operators";
 
 describe('NormalComponent', () => {
   let component: NormalComponent;
@@ -33,5 +35,56 @@ describe('NormalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call invalid', () => {
+    const email = {value: 'test', invalid: false};
+    const passw = {value: 'hi', invalid: false};
+    // @ts-ignore
+    const res1 = component.invalid(email, passw, passw);
+    expect(res1).toEqual(false);
+    email.invalid = true;
+    // @ts-ignore
+    expect(component.invalid(email, passw, passw)).toEqual(true);
+  });
+
+  it('should call onSuccess', () => {
+    const spy = spyOn(component.loginService, 'normalLogin').and.returnValue(of({}).pipe(map(_ => {
+      throw new Error('unlucky');
+    })));
+    const spy2 = spyOn(component.service, 'justRegistered');
+    const spy3 = spyOn(component.router, 'navigate');
+    const email = {value: 'email'};
+    const passw = {value: 'passw'};
+    const sub = {unsubscribe: () => {}};
+    // @ts-ignore
+    component.onSuccess('some str', sub, email, passw);
+    expect(spy).toHaveBeenCalled();
+    expect(spy2).not.toHaveBeenCalled();
+
+    spy.and.returnValue(of({}));
+    // @ts-ignore
+    component.onSuccess('some str', sub, email, passw);
+    expect(spy2).toHaveBeenCalled();
+    expect(spy3).toHaveBeenCalled();
+  });
+
+  it('should handle click', () => {
+    component.loading = false;
+    const spy = spyOn(component, 'invalid').and.returnValue(true);
+    const spy2 = spyOn(component.http, 'post').and.returnValue(of('str'));
+    const spy3 = spyOn(component, 'onSuccess');
+    component.onClick(undefined, undefined, undefined);
+    expect(spy).toHaveBeenCalled();
+    expect(component.loading).toEqual(false);
+    spy.and.returnValue(false);
+    spy.calls.reset();
+    const someObj = {value: 'idk'};
+    // @ts-ignore
+    component.onClick(someObj, someObj, someObj);
+    expect(spy).toHaveBeenCalled();
+    expect(component.loading).toEqual(true);
+    expect(spy2).toHaveBeenCalled();
+    // expect(spy3).toHaveBeenCalled();
   });
 });
--- UGWFrontend/src/app/modules/setup/register/normal/normal.component.ts
+++ UGWFrontend/src/app/modules/setup/register/normal/normal.component.ts
@@ -7,6 +7,7 @@ import {SetupService} from '../../state/setup.service';
 import {LoginService} from '../../login/login.service';
 import {Router} from '@angular/router';
 import {handleError} from 'src/app/util';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-normal',
@@ -16,37 +17,42 @@ import {handleError} from 'src/app/util';
 export class NormalComponent {
 
   constructor(public query: SetupQuery,
-              private service: SetupService,
-              private http: HttpClient,
-              private loginService: LoginService,
-              private router: Router) { }
+              public service: SetupService,
+              public http: HttpClient,
+              public loginService: LoginService,
+              public router: Router) { }
 
   loading = false;
   error: string = null;
 
   onClick(email: InputComponent, passw: InputComponent, passwwdh: InputComponent) {
-    if (email.value === '' || passw.value === '' || email.invalid || passw.invalid || passw.value !== passwwdh.value) {
+    if (this.invalid(email, passw, passwwdh)) {
       return;
     }
     this.loading = true;
-
-    const sub = this.http.post(environment.urls.registerNormal, {
+    let sub = null;
+    sub = this.http.post(environment.urls.registerNormal, {
       fullname: this.query.getValue().name,
       email: email.value,
       password: passw.value
-    }).subscribe((data: string) => {
-        sub.unsubscribe();
-        console.log(data);
-        this.loginService.normalLogin(email.value, passw.value).subscribe(
-          (_) => {
-            this.service.justRegistered();
-            this.router.navigate(['/setup/basics']);
-
-          },
-          (error) => handleError(this, error)
-        );
-      },
+    }).subscribe(
+      (data: string) => this.onSuccess(data, sub, email, passw),
       (error) => handleError(this, error)
     );
+  }
+
+  onSuccess(data: string, sub: Subscription, email: InputComponent, passw: InputComponent) {
+    sub.unsubscribe();
+    console.log(data);
+    this.loginService.normalLogin(email.value, passw.value).subscribe((_) => {
+        this.service.justRegistered();
+        this.router.navigate(['/setup/basics']);
+      },
+  (error) => handleError(this, error)
+    );
+  }
+
+  invalid(email: InputComponent, passw: InputComponent, passwwdh: InputComponent) {
+    return email.value === '' || passw.value === '' || email.invalid || passw.invalid || passw.value !== passwwdh.value;
   }
 }
