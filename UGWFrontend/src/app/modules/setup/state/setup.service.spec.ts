cc5013d18d51544fc772819dc3bac8ebda7ec0e1
FoseFx <fosefx@pm.me>
2019-08-13 17:43:43 +0200

    Added tests for SetupService

--- UGWFrontend/src/app/modules/setup/state/setup.service.spec.ts
+++ UGWFrontend/src/app/modules/setup/state/setup.service.spec.ts
@@ -1,5 +1,5 @@
 import {TestBed} from '@angular/core/testing';
-import {SetupService} from './setup.service';
+import {kurseSortFn, SetupService} from './setup.service';
 import {SetupStore} from './setup.store';
 import {SetupQuery} from './setup.query';
 
@@ -67,5 +67,41 @@ describe('SetupService', () => {
     expect(spy).toHaveBeenCalled();
   });
 
+  it('should sort kurse', () => {
+    // @ts-ignore
+    expect(kurseSortFn({title: 'LKD2'}, {title: 'LKM1'})).toEqual(-1);
+    // @ts-ignore
+    expect(kurseSortFn({title: 'LKD2'}, {title: 'M1'})).toEqual(-1);
+    // @ts-ignore
+    expect(kurseSortFn({title: 'D2'}, {title: 'LKM1'})).toEqual(1);
+    // @ts-ignore
+    expect(kurseSortFn({title: 'B2'}, {title: 'A1'})).toEqual(1);
+    // @ts-ignore
+    expect(kurseSortFn({title: 'A2'}, {title: 'B1'})).toEqual(-1);
+    // @ts-ignore
+    expect(kurseSortFn({title: 'A1'}, {title: 'A1'})).toEqual(0);
+  });
+
+  it('should setAvailableKurse', () => {
+    const service: SetupService = TestBed.get(SetupService);
+    const spy = spyOn(service.store, 'update').and.callFake(v => {
+      console.log('abcdefg', v);
+      expect(v).toEqual({
+        available_kurse: {
+          title1: [
+            {fach: 'Frei', lehrer: '', title: 'title1'},
+            {title: 'title1', lehrer: 'l1', fach: 'f1'},
+            {title: 'title1', lehrer: 'l1', fach: 'f1'}
+            ]
+        },
+        selected_kurse_indexes: {title1: -1}
+      });
+    });
+    service.setAvailableKurse([
+      {title: 'title1', lehrer: 'l1', fach: 'f1'},
+      {title: 'title1', lehrer: 'l1', fach: 'f1'}
+    ]);
+    expect(spy).toHaveBeenCalled();
+  });
 });
 
